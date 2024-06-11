701cca885ef1016a3015ef7e5517714fa59fdc9b
Md-Azmir <azmir.0dbg.123@gmail.com>
2024-06-11 22:13:48 +0530

    first sommit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
+
   return (
-    <div className="App">
+    <div className={isDarkMode ? 'dark-mode' : 'light-mode'}>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Welcome to My App</h1>
+        <button onClick={toggleDarkMode}>
+          {isDarkMode?("Toggle Light Mode"):("Toggle Dark Mode")}
+        </button>
       </header>
     </div>
   );
